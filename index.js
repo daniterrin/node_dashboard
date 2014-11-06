2154dfe0cd43d463c2a60d7f62ed55366b4b47bc
daniterrin <dani.terrin@gmail.com>
2014-11-06 17:11:09 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-var app = require('./server');
-
-var server = app.listen(3000, function () {
-	var host = server.address().address;
-	var port = server.address().port;
-
-	console.log('App listening at http://%s:%s', host, port);
-})
