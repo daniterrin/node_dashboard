4ab0d04194ef4de5cd6b0a17cf571b33d98eff81
unknown <daniel.terrin@ST-L215-FLTT9S1.domino.softonic.com>
2014-11-06 16:33:14 +0100

    Moving JS logic out of index.html

---
+++ client/public/js/main.js
@@ -0,0 +1,82 @@
+var DATA_NOT_FOUND_STATUS = { message: '<b>No data was found for these sprints</b>', className: 'alert alert-warning', hide: false };
+var ERROR_STATUS = { message: '<b>No data was found for these sprints</b>', className: 'alert alert-danger', hide: false };
+var LOADING_STATUS = { message: '<b>Attention!</b> chart is loading...you might want a coffee', className: 'alert alert-info', hide: false };
+var HIDE_STATUS = { message: '', className: '', hide: true };
+
+function loadData() {
+	var sprints = document.getElementById('sprints').value;
+	var loading = document.getElementById('loading');
+	var jiraURL = "data/jiraDefault";
+	if(sprints != "") {
+		jiraURL = "data/jira?versions=" + sprints
+	}
+	$.getJSON(jiraURL,function(resp){
+		processResponse(resp);
+	});
+	setLoadingStatus(LOADING_STATUS);
+}
+
+function processResponse(resp) {
+	dataProvider = [];
+	trendLines = [];
+	var status = null;
+	
+	if (typeof(resp.error) == "undefined") {
+		if (resp.data.length > 0) {
+			dataProvider = resp.data;
+			trendLines = [resp.trendline];
+			status = HIDE_STATUS;
+		} else {
+			status = DATA_NOT_FOUND_STATUS;
+		}
+	}
+	else {
+		status = ERROR_STATUS;
+	}
+	drawChart(dataProvider, trendLines);
+	setLoadingStatus(status);
+}
+
+function drawChart(dataProvider, trendLines) {
+	AmCharts.makeChart("chartdiv", {
+		"type": "serial",
+		"theme": "light",
+		"autoMargins": false,
+		"marginLeft":30,
+		"marginRight":8,
+		"marginTop":10,
+		"marginBottom":26,
+		"handDrawn": true,
+				
+		"valueAxes": [{
+			"axisAlpha": 0,
+			"position": "left",
+			"minimum": 0
+		}],
+		"startDuration": 1,
+		"graphs": [{
+			"fillAlphas": 1,
+			"title": "storyPoints",
+			"type": "column",
+			"valueField": "storyPoints"
+		}],
+		"categoryField": "sprint",
+		"categoryAxis": {
+			"gridPosition": "start",
+			"axisAlpha":0,
+			"tickLength":0
+		},
+		"dataProvider": dataProvider,
+		"trendLines": trendLines
+	});	
+}
+
+function setLoadingStatus(status) {
+	
+	loading.innerHTML = status.message;
+	loading.className = status.className;
+	loading.hidden = status.hide;
+}
+
+// Main
+loadData();
